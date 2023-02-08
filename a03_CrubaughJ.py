1c244ac36aae089ad79ec65ac4bbba5593671f5d
CrubaughJ <CrubaughJ@berea.edu>
2023-02-07 21:23:33 -0500

    checkpoint

--- a03_CrubaughJ.py
+++ a03_CrubaughJ.py
@@ -1,64 +1,93 @@
+# HEADER
+
+# David Crubaugh's Fox Drawing (CrubaughJ)
+# Google Doc: https://docs.google.com/document/d/1Ge_fdmXzmHLe7J_Es64Gu3SUcqonJlOVE1zQhTHO1jQ/edit?usp=sharing
+
+# ----------------------------------------------------------------------------------------------------------------------
+# The first thing I did was to import turtle and to create the Screen and turtle (I also set the color of both)
+# ----------------------------------------------------------------------------------------------------------------------
+
 import turtle
 
 wn = turtle.Screen()
-wn.bgcolor("black")
+wn.bgcolor('#000000')
 fox = turtle.Turtle()
-fox.color("orange")
+fox.color('#FFA500')
 
 
-def eqtri(x, y, s, a, c):
+# ----------------------------------------------------------------------------------------------------------------------
+# fill(x, y, c) fills the shape I found that I reused this code 3x, so I made a function for it.
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def fill(x, y, c):
     fox.fillcolor(c)
     fox.penup()
     fox.goto(x, y)
     fox.begin_fill()
     fox.pendown()
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# This was the first function I created it creates an equilateral triangle and calls the function to fill the triangle.
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def eq_tri(x, y, s, a, c):
+    fill(x, y, c)
     for i in range(0, 3):
         fox.forward(s)
         fox.right(a)
     fox.end_fill()
 
 
-def nose(x, y, t):
-    fox.penup()
-    fox.goto(0, 0)
-    fox.pendown()
-    fox.goto(x, y)
-    fox.fillcolor("white")
-    fox.begin_fill()
-    fox.forward(t)
-    fox.forward(2 * -t)
-    fox.right(60)
-    fox.forward(40)
-    fox.left(120)
-    fox.forward(40)
-    fox.end_fill()
+# ----------------------------------------------------------------------------------------------------------------------
+# This was the second function I created which makes the eyes of the fox by creating a circle.
+# ----------------------------------------------------------------------------------------------------------------------
 
 
-def cir(x, y, d):
-    fox.penup()
-    fox.goto(x, y)
-    fox.pendown()
-    fox.fillcolor("black")
-    fox.begin_fill()
-    fox.circle(d)
-    fox.end_fill()
+def cir(x, y, d, c):
+    fill(x, y, c)
+    for i in range(1):
+        fox.circle(d)
+        fox.end_fill()
 
-def main():
-    eqtri(-75, 0, 150, 120, "orange")
-    eqtri(-75, 0, 40, -120, "orange")
-    eqtri(-70, 0, 30, -120, "white")
-    eqtri(35, 0, 40, -120, "orange")
-    eqtri(40, 0, 30, -120, "white")
 
-    nose(0, -95, 20)
+# ----------------------------------------------------------------------------------------------------------------------
+# As the name implies this gets ride of the turtle.
+# ----------------------------------------------------------------------------------------------------------------------
 
-    cir(-20, -30, 5)
-    cir(20, -30, 5)
 
+def get_ride_of_fox():
     fox.penup()
     fox.goto(300, 300)
-    fox.color("black")
+    fox.color('#000000')
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# This is where I plug in the colors and the placement of the different functions as well as there size.
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def main():
+    eq_tri(-75, 0, 150, 120, '#FFA500')
+    eq_tri(-75, 0, 40, -120, '#FFA500')
+    eq_tri(-70, 0, 30, -120, '#FFFFFF')
+    eq_tri(35, 0, 40, -120, '#FFA500')
+    eq_tri(40, 0, 30, -120, '#FFFFFF')
+    eq_tri(-20, -95, 40, 120, '#FFFFFF')
+
+    cir(-20, -30, 5, '#000000')
+    cir(20, -30, 5, '#000000')
+
+    get_ride_of_fox()
+
     wn.exitonclick()
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# I call main. This is my entire code.
+# ----------------------------------------------------------------------------------------------------------------------
 
 
 main()
