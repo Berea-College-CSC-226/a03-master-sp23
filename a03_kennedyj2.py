4e6ece58e45a0c1e228242c8ba405aab90a7bc53
kennedyj2 <kennedyj2@berea.edu>
2023-02-07 22:01:35 -0500

    this was difficult

--- a03_kennedyj2.py
+++ a03_kennedyj2.py
@@ -3,12 +3,12 @@
 ###############################################################################################################
 from turtle import *
 
-def main(): #function
+def fleur(): #function
     for i in range(300): #creates the flower
         circle(190-i,90)
         left(90)
         circle(190-i,90)
         left(18)
 
-main()
+fleur()
 mainloop()
