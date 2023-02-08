c6d2021d8b3606a8d67857da55ba39d029a9cd1e
nailevr <nailevr@berea.edu>
2023-02-07 20:01:26 -0500

    added code "late night date " with stars on the sky

--- nailevr.py
+++ nailevr.py
@@ -22,7 +22,7 @@ yo = turtle.Turtle()
 
 
 
-def randomizing(number_of_stars, size, color):
+def randomizing(number_of_stars, size, color):  #making starts appear in random spots
 
     for e in range(number_of_stars):
         yo.penup()
@@ -30,7 +30,7 @@ def randomizing(number_of_stars, size, color):
         yo.pendown()
         star(size, color)
 
-def star(size, color):
+def star(size, color): #draw a star
     yo.color(color)
     yo.begin_fill()
     for i in range(5):
@@ -38,7 +38,7 @@ def star(size, color):
         yo.right(144)
     yo.end_fill()
 
-def person1(personsize):
+def person1(personsize): #draw person1
     yo.penup()
     yo.color("white")
     yo.pensize(10)
@@ -58,7 +58,7 @@ def person1(personsize):
     yo.left(20)
     yo.forward(personsize/4)
 
-def person2(personsize):
+def person2(personsize): #draw person2
     yo.penup()
     yo.color("white")
     yo.pensize(10)
@@ -71,7 +71,7 @@ def person2(personsize):
     yo.circle(personsize/4)
     yo.end_fill()
 
-def text():
+def text(): #write a text
     yo.penup()
     yo.goto(200,-200)
     yo.write("Late Night Date", font=("Verdana", 15, "normal"))
@@ -81,7 +81,7 @@ def text():
 
 
 
-def main():
+def main(): #important
 
     wn = turtle.Screen()
     wn.colormode(255)
